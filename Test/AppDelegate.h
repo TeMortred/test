ad609c8e8b40148f1280f23f1ff35a4bf185be8d
TeMortred <TeMortred@TeMortredMacBook-Pro.local>
2017-05-11 09:33:08 +0800

    第一次提交

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by TeMortred on 17/5/11.
+//  Copyright © 2017年 TeMortred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by TeMortred on 17/5/11.
+//  Copyright © 2017年 TeMortred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
